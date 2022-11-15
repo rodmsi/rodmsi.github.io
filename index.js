397303cf535a903020b8717148ef6e4fba5a07ca
rodmsi <118315870+rodmsi@users.noreply.github.com>
2022-11-15 19:39:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const mobileNav = document.querySelector('.mobile-navigation ul');
+
+document.querySelector('.mobile-navigation i').addEventListener('click',
+	() => {
+		mobileNav.classList.contains('show')
+	? mobileNav.classList.remove('show')
+	: mobileNav.classList.add('show');
+	}
+);
+
+ScrollReveal().reveal(".cool-stuff-row");
